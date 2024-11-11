6a51fb3909d9d5cf5bcf392fa8bf048d926d635a
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2024-11-11 14:55:06 -0600

    validaciones del inicio de sesión

--- mis_videos/views.py
+++ mis_videos/views.py
@@ -1,23 +1,54 @@
 #from django.shortcuts import render
 # Create your views here.
+import re
 from django.http import HttpResponse
 from .models import Video,Usuario
 from django.shortcuts import render,redirect
-from validaciones.validaciones import validar_nombre_usuario,validar_usuario_id
+#from validaciones.validaciones import validar_nombre_usuario,validar_usuario_id
+#
+def validar_usuario_id(numero_nomina):
+    # Valor alfanumérico (A-Z, a-z, 0-9)
+    if re.fullmatch(r"[A-Za-z0-9]+", numero_nomina):
+        return True
+    print("Número de nómina inválido. Debe ser alfanumérico (A-Z, a-z, 0-9).")
+    return False
+#
+def validar_nombre_usuario(nombre_usuario):
+    # Valor alfabético (A-Z, a-z)
+    if re.fullmatch(r"[A-Za-z]+", nombre_usuario):
+        return True
+    print("Nombre del usuario inválido. Solo debe contener letras (A-Z, a-z).")
+    return False
+
 
 def index(request):
+    mensajeParaID = False
+    mensajeParaNombre = False
+    
     if request.method == "POST":
         # Obtener los datos del formulario
         usuario_id = request.POST.get("usuarioID")
         usuario_nombre = request.POST.get("usuarioNombre")
-        
+        if validar_usuario_id(usuario_id) == False:
+            mensajeParaID = True
+        if validar_nombre_usuario(usuario_nombre) == False:
+            mensajeParaNombre = True
+
+        # Si hay algún error de validación, renderiza con los mensajes de error
+        if mensajeParaID or mensajeParaNombre:
+            return render(request, "index.html", {
+                "mensajeParaID": mensajeParaID,
+                "mensajeParaNombre": mensajeParaNombre
+            })
+
         # Crear o recuperar el usuario desde la base de datos
         usuario, existe = Usuario.objects.get_or_create(usuarioID=usuario_id, usuarioNombre=usuario_nombre)
         
         # Redirigir a la interfaz de usuario con el ID del usuario
         return redirect('interfazUsuario', user=usuario)
     
-    return render(request, "index.html")
+    return render(request, "index.html", {"mensajeParaID": mensajeParaID, "mensajeParaNombre": mensajeParaNombre})
+
 
 
 def interfazUsuario(request, user):
