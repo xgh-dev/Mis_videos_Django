20ed27dcb601ac7679afc5d4539415efbb779600
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2024-11-22 12:20:10 -0600

    clase implementada para las validaciones

--- mis_videos/views.py
+++ mis_videos/views.py
@@ -1,51 +1,50 @@
 #para hacer consulta usamos import psycopg2 y definimos las conexiones y el cursor para proceder a desarrollar las consultas
 # Create your views here.
 import re
-#from django.http import HttpResponse
 from .models import Video,Usuario
 from django.shortcuts import render,redirect
 # redirect dirige el usuario a otra URL y se utiliza el request de la vista en la cual se ejecuta, redirige al usuario a otra URL o vista, como si se hiciera una nueva solicitud.
 # render, renderiza una plantilla y la muestra en la misma solicitud
 #Usa render cuando quieras presentar una página directamente, y redirect cuando sea necesario redirigir al usuario a otro lugar
 
-#from validaciones import validar_usuario_id,validar_nombre_usuario,validar_nombre_video,validar_tamaño,validar_extension_video
+class Validaciones():
+    def __init__(self,elemento_para_validar):
+        self.elemento_para_validar = elemento_para_validar
+    
+    def validar_usuario_id(self):
+        # Valor alfanumérico (A-Z, a-z, 0-9)
+        if re.fullmatch(r"[A-Za-z0-9]+", self.elemento_para_validar):
+            return True
+        print("Número de nómina inválido. Debe ser alfanumérico (A-Z, a-z, 0-9).")
+        return False
 
+    def validar_nombre_usuario(self):
+        # Valor alfabético (A-Z, a-z)
+        if re.fullmatch(r"[A-Za-z]+", self.elemento_para_validar):
+            return True
+        print("Nombre del usuario inválido. Solo debe contener letras (A-Z, a-z).")
+        return False
 
-def validar_usuario_id(numero_nomina):
-    # Valor alfanumérico (A-Z, a-z, 0-9)
-    if re.fullmatch(r"[A-Za-z0-9]+", numero_nomina):
-        return True
-    print("Número de nómina inválido. Debe ser alfanumérico (A-Z, a-z, 0-9).")
-    return False
+    def validar_nombre_video(self):
+        # Valor alfanumérico (A-Z, a-z, 0-9 )
+        if re.fullmatch(r"[A-Za-z0-9 ]+", self.elemento_para_validar):
+            return True
+        print("Nombre del video inválido. Debe ser alfanumérico (A-Z, a-z, 0-9).")
+        return False
 
-def validar_nombre_usuario(nombre_usuario):
-    # Valor alfabético (A-Z, a-z)
-    if re.fullmatch(r"[A-Za-z]+", nombre_usuario):
-        return True
-    print("Nombre del usuario inválido. Solo debe contener letras (A-Z, a-z).")
-    return False
+    def validar_extension_video(self):
+        # Valor alfanumérico (A-Z, a-z, 0-9) seguido de .com
+        if re.fullmatch(r"[A-Za-z0-9]+\.(com)", self.elemento_para_validar):
+            return True
+        print("Extensión del video inválida. Debe ser alfanumérica seguida de '.com'.")
+        return False
 
-def validar_nombre_video(nombre_video):
-    # Valor alfanumérico (A-Z, a-z, 0-9 )
-    if re.fullmatch(r"[A-Za-z0-9 ]+", nombre_video):
-        return True
-    print("Nombre del video inválido. Debe ser alfanumérico (A-Z, a-z, 0-9).")
-    return False
-
-def validar_extension_video(extension_video):
-    # Valor alfanumérico (A-Z, a-z, 0-9) seguido de .com
-    if re.fullmatch(r"[A-Za-z0-9]+\.(com)", extension_video):
-        return True
-    print("Extensión del video inválida. Debe ser alfanumérica seguida de '.com'.")
-    return False
-
-def validar_tamaño(tamano):
-    # Valor numérico (0-3)
-    if re.fullmatch(r"[0-3]", tamano):
-        return True
-    print("Tamaño inválido. Debe ser un número entre 0 y 3.")
-    return False
-
+    def validar_tamaño(self):
+        # Valor numérico (0-3)
+        if re.fullmatch(r"[0-3]", self.elemento_para_validar):
+            return True
+        print("Tamaño inválido. Debe ser un número entre 0 y 3.")
+        return False
 
 def index(request):
     mensajeParaID = False
@@ -55,9 +54,9 @@ def index(request):
         # Obtener los datos del formulario
         usuario_id = request.POST.get("usuarioID")
         usuario_nombre = request.POST.get("usuarioNombre")
-        if validar_usuario_id(usuario_id) == False:
+        if Validaciones(usuario_id).validar_usuario_id() == False:
             mensajeParaID = True
-        if validar_nombre_usuario(usuario_nombre) == False:
+        if Validaciones(usuario_nombre).validar_nombre_usuario() == False:
             mensajeParaNombre = True
 
         # Si hay algún error de validación, renderiza con los mensajes de error
@@ -90,11 +89,11 @@ def interfazUsuario(request, user):
         nombreVideo = request.POST.get("nombreVideo")
         rutaVideo = request.POST.get("rutaVideo")
         tamañoVideo = request.POST.get("videoTamaño")
-        if validar_nombre_video(nombreVideo) == False:
+        if Validaciones(nombreVideo).validar_nombre_video() == False:
             mensajeNombreVideo = True
-        if validar_extension_video(rutaVideo) == False:
+        if Validaciones(rutaVideo).validar_extension_video() == False:
             mensajeRutaVideo = True
-        if validar_tamaño(tamañoVideo) == False:
+        if Validaciones(tamañoVideo).validar_tamaño() == False:
             mensajeTamañoVideo = True
         if mensajeNombreVideo or mensajeRutaVideo or mensajeTamañoVideo:
             return render(request, "interfazDeUsuario.html", {
