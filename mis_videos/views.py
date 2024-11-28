439715768522da8f972aa6077f98160a2a0db1e9
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2024-11-27 19:29:46 -0600

    entrega

--- mis_videos/views.py
+++ mis_videos/views.py
@@ -1,10 +1,11 @@
-#para hacer consulta usamos import psycopg2 y definimos las conexiones y el cursor para proceder a desarrollar las consultas
-# Create your views here.
+#Create your views here.
 import re
 from .models import Video,Usuario
 from django.shortcuts import render,redirect
-# redirect dirige el usuario a otra URL y se utiliza el request de la vista en la cual se ejecuta, redirige al usuario a otra URL o vista, como si se hiciera una nueva solicitud.
-# render, renderiza una plantilla y la muestra en la misma solicitud
+
+#Redirect dirige el usuario a otra URL y se utiliza el request de la vista en la cual se ejecuta, redirige al usuario a otra URL o vista, como si se hiciera una nueva solicitud.
+
+#Render, renderiza una plantilla y la muestra en la misma solicitud
 #Usa render cuando quieras presentar una página directamente, y redirect cuando sea necesario redirigir al usuario a otro lugar
 
 class Validaciones():
@@ -99,7 +100,7 @@ def index(request):
     mensajeParaNombre = False
     
     if request.method == "POST":
-        # Obtener los datos del formulario
+        #Obtener los datos del formulario
         usuario_id = request.POST.get("usuarioID")
         usuario_nombre = request.POST.get("usuarioNombre")
         if Validaciones(usuario_id).validar_usuario_id() == False:
@@ -107,17 +108,17 @@ def index(request):
         if Validaciones(usuario_nombre).validar_nombre_usuario() == False:
             mensajeParaNombre = True
 
-        # Si hay algún error de validación, renderiza con los mensajes de error
+        #Si hay algún error de validación, renderiza con los mensajes de error
         if mensajeParaID or mensajeParaNombre:
             return render(request, "index.html", {
                 "mensajeParaID": mensajeParaID,
                 "mensajeParaNombre": mensajeParaNombre
             })
 
-        # Crear o recuperar el usuario desde la base de datos
+        #Crear o recuperar el usuario desde la base de datos
         usuario, existe = Usuario.objects.get_or_create(usuarioID=usuario_id, usuarioNombre=usuario_nombre)
         
-        # Redirigir a la interfaz de usuario con el ID del usuario
+        #Redirigir a la interfaz de usuario con el ID del usuario
         return redirect('interfazUsuario', user=usuario)
     
     return render(request, "index.html", {"mensajeParaID": mensajeParaID, "mensajeParaNombre": mensajeParaNombre})
@@ -133,10 +134,10 @@ def interfazUsuario(request, user):
     Returns:
         HttpResponse: La respuesta con el renderizado de la página y los videos filtrados.
     """
-    # Recuperar el usuario por su ID
+    #Recuperar el usuario por su ID
     usuario = Usuario.objects.get(usuarioID=user)
     
-    # Obtener los videos de ese usuario
+    #Obtener los videos de ese usuario
     videos = Video.objects.filter(usuario=usuario)
     
     mensajeNombreVideo = False
@@ -160,20 +161,20 @@ def interfazUsuario(request, user):
                 "mensajeRutaVideo": mensajeRutaVideo,
                 "mensajeTamañoVideo": mensajeTamañoVideo,   
             })
-        # Validamos que todos los campos requeridos están completos
-        # tamañoVideo = float(tamañoVideo)  # Convertimos a float si tiene valor válido
-        # Crea y guarda el nuevo video en la base de datos
+        #Validamos que todos los campos requeridos están completos
+        #tamañoVideo = float(tamañoVideo)  # Convertimos a float si tiene valor válido
+        #Crea y guarda el nuevo video en la base de datos
         nuevo_video = Video(
                 videoNombre=nombreVideo,
                 videoRuta=rutaVideo,
                 videoTamaño=float(tamañoVideo),
-                usuario=usuario  # Aquí estamos usando el objeto `usuario` recuperado
+                usuario=usuario #Aquí estamos usando el objeto `usuario` recuperado
                 )
         nuevo_video.save()
         return render(request, "interfazDeUsuario.html", {
                 "videos": videos,   
             })
-    # Renderizamos el HTML completo con el mensaje y los videos filtrados
+    #Renderizamos el HTML completo con el mensaje y los videos filtrados
     return render(request, "interfazDeUsuario.html", {
                 "videos": videos,  
             })
@@ -203,6 +204,6 @@ def eliminarVideo(request, NombreDelVideo):
         HttpResponse: La respuesta redirigiendo a la interfaz de usuario del video eliminado.
     """
     video = Video.objects.get(videoNombre=NombreDelVideo)
-    usuario = video.usuario  # Extraemos el id del usuario de la variable video
+    usuario = video.usuario #Extraemos el id del usuario de la variable video
     video.delete()
     return redirect('interfazUsuario', user=usuario)
